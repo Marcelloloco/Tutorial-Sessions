03d127255f3288095fb102a11162cb46fda3fbae
Marcel Trattner <m.trattner99@gmail.com>
2017-11-20 15:30:37 +0100

    insert, delete, search function added in unsortedarray.java

--- src/main/java/htw/tutorium/Datastructures/App.java
+++ src/main/java/htw/tutorium/Datastructures/App.java
@@ -1,5 +1,7 @@
 package htw.tutorium.Datastructures;
 
+import htw.tutorium.arrays.UnsortedArray;
+
 /**
  * Hello world!
  *
@@ -8,7 +10,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hallooo" );
+        UnsortedArray u = new UnsortedArray(8);
+        u.insert(6);
+        u.insert(7);
+        u.insert(74);
+        u.insert(1);
+        
+        
+        int[] tmp = u.returnArray();
+        for(int i = 0; i<tmp.length; i++) {
+        	System.out.println(tmp[i]);
+        }
+        u.delete(74);
+        
+        tmp = u.returnArray();
+        for(int i = 0; i<tmp.length; i++) {
+        	System.out.println(tmp[i]);
+        }
     }
     
     
--- src/main/java/htw/tutorium/arrays/UnsortedArray.java
+++ src/main/java/htw/tutorium/arrays/UnsortedArray.java
@@ -2,8 +2,43 @@ package htw.tutorium.arrays;
 
 public class UnsortedArray {
 
-	public UnsortedArray() {
-		
+	private int[] unsorted;
+	private int index;
+	
+	public UnsortedArray(int i) {
+		unsorted = new int[i];
+		index = 0;
 	}
+	
+	public void insert(int elem) {
+		unsorted[index] = elem;
+		index += 1;
+	}
+	
+	public int search(int j) {
+		
+		for(int i = 0; i < unsorted.length; i++) {
+			if(unsorted[i] == j) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public void delete(int a) {
+		int deleteIndex = search(a);
+		for(int i = deleteIndex; i < unsorted.length; i++) {
+			if(i  < (unsorted.length - 1)) {
+				unsorted[i] = unsorted[ i + 1];
+			}
+		}
+		index--;
+	}
+	
+	
+	public int[] returnArray() {
+		return unsorted;
+	}
+	
 	
 }
