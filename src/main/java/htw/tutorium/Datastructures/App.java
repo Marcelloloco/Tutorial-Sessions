9773aa477e3931fcf795dcfbb676c5971c4bc7d9
Marcel Trattner <m.trattner99@gmail.com>
2017-11-20 13:02:44 +0100

    initial commit

---
+++ src/main/java/htw/tutorium/Datastructures/App.java
@@ -0,0 +1,15 @@
+package htw.tutorium.Datastructures;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    
+}
